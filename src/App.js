181e55445b90f7662aa50c6726a76a0b9e497f0f
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2023-08-28 20:44:14 +0200

    Upload changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { colorModeContext, useMode } from "./theme";
+import { CssBaseline, Menu, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
-import Dashboard from "./scenes/dashboard";
+import Dashboard from "./scenes/dashboard/index.jsx";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
-/*import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
-import Calender from "./scenes/calender";*/
-import { colorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import Calendar from "./scenes/calendar/calendar";
+import { MenuItem, } from 'react-pro-sidebar';
+{/* import Geography from "./scenes/geography";*/ }
+
+
 
 function App() {
 
@@ -26,23 +29,24 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
-          <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              {/*<Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/calender" element={<Calender />} />*/}
-            </Routes>
-          </main>
+          <Sidebar isSidebar={isSidebar} >
+            <main className="content">
+              <Topbar setIsSidebar={setIsSidebar} />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/faq" element={<FAQ />} />
+                {/*<Route path="/geography" element={<Geography />} />*/}
+                <Route path="/calendar" element={<Calendar />} />
+              </Routes>
+            </main>
+          </Sidebar>
         </div>
       </ThemeProvider>
     </colorModeContext.Provider>
